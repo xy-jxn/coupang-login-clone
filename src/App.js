01c2464e5f789e9da152becaccb6ae724649835b
xy-jxn <s24016@gsm.hs.kr>
2024-12-16 08:55:30 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Routes>
+        
+      </Routes>
+    </Routes>
+  );
+}
+
+export default App;
