209c5a3f897599f5803d415be97a332098306a4a
xy-jxn <s24016@gsm.hs.kr>
2024-12-16 20:04:22 +0900

    Restart

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import EmailLogin from "./pages/EmailLogin";
-import PhoneNumberLogin from "./pages/PhoneNumberLogin";
-import QRcodeLogin from "./pages/QRcodeLogin";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/email" element={<EmailLogin />} />
-        <Route path="/phonenumber" element={<PhoneNumberLogin />} />
-        <Route path="/QRcode" element={<QRcodeLogin />} />
+        
       </Routes>
     </Router>
   );
