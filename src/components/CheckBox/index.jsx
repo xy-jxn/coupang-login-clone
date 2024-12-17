ed352bd4039d2f5881f197b27b96b0834a806f39
xy-jxn <s24016@gsm.hs.kr>
2024-12-17 23:58:26 +0900

    CheckBox onclick 했을 때 색깔 변경 구현

--- src/components/CheckBox/index.jsx
+++ src/components/CheckBox/index.jsx
@@ -1,8 +1,16 @@
 import * as S from "./style";
+import React, { useState } from "react";
 import Check from "../../assets/check.svg";
 
 function CheckBox() {
-  return <S.Box src={Check}/>
+  const [check, setCheck] = useState(false);
+  
+  const handleChack = () => {
+    setCheck(!check);
+    console.log(check);
+  };
+
+  return <S.Box src={Check} onClick={handleChack} check={check} />;
 }
 
 export default CheckBox;
--- src/components/CheckBox/style.js
+++ src/components/CheckBox/style.js
@@ -3,7 +3,10 @@ import styled from "styled-components";
 export const Box = styled.img`
   width: 20px;
   height: 20px;
-  border: 1px solid #666;
   border-radius: 20%;
   margin-right: 8px;
+
+  ${(props) => props.check ? "border: 1px solid #556BF5" : "border: 1px solid #666"};
+
+  ${(props) => props.check ? "background-color: #556BF5" : ""};
 `;
